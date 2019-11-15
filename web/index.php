f0b43c6240523437339c0559bd224b58a6536b34
satosan2 <tekito@pino2.tank.jp>
2019-11-15 21:18:31 +0900

    a

---
+++ web/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>macho</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+maccho
+</body>
+</html>
